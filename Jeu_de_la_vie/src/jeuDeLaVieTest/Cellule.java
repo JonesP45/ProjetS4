34f0ce18cbe931bd1d301289026594dfcd22f6fc
J-P <jeanpacome.delmas@hotmail.fr>
2018-02-14 14:14:49 +0100

    Test

--- Jeu_de_la_vie/src/jeuDeLaVieTest/Cellule.java
+++ Jeu_de_la_vie/src/jeuDeLaVieTest/Cellule.java
@@ -2,12 +2,12 @@ package jeuDeLaVieTest;
 
 public class Cellule implements Comparable<Object>{
 
-    private int x;
-    private int y;
+    private int ligne;
+    private int colonne;
 
-    public Cellule(int x, int y) {
-        this.x = x;
-        this.y = y;
+    public Cellule(int ligne, int colonne) {
+        this.ligne = ligne;
+        this.colonne = colonne;
     }
 
     @Override
@@ -17,46 +17,64 @@ public class Cellule implements Comparable<Object>{
         if (!(o instanceof Cellule))
             return false;
         Cellule cellule = (Cellule) o;
-        return getX() == cellule.getX() && getY() == cellule.getY();
+        return getLigne() == cellule.getLigne() && getColonne() == cellule.getColonne();
     }
+
+//    @Override
+//    public int compareTo(Object o) {
+//        if (!(o instanceof Cellule))
+//            throw new IllegalArgumentException("L'objet en paramètre n'est pas une cellule");
+//        Cellule cellule = (Cellule)o;
+//        if (ligne < cellule.getLigne())
+//            return -1;
+//        else if (ligne > cellule.getLigne())
+//            return 1;
+//        else {
+//            if (colonne > cellule.getColonne())
+//                return 1;
+//            else if (colonne < cellule.getColonne())
+//                return -1;
+//            else
+//                return 0;
+//        }
+//    }
 
     @Override
     public int compareTo(Object o) {
         if (!(o instanceof Cellule))
             throw new IllegalArgumentException("L'objet en paramètre n'est pas une cellule");
         Cellule cellule = (Cellule)o;
-        if (x < cellule.getX())
+        if (ligne > cellule.getLigne())
             return -1;
-        else if (x > cellule.getX())
+        else if (ligne < cellule.getLigne())
             return 1;
         else {
-            if (y > cellule.getY())
+            if (colonne > cellule.getColonne())
                 return 1;
-            else if (y < cellule.getY())
+            else if (colonne < cellule.getColonne())
                 return -1;
-            else
-                return 0;
         }
+        return 0;
     }
 
     public String toString() {
-        return "[" + x + ", " + y + "]";
+        return "[" + ligne + ", " + colonne + "]";
     }
 
-    public int getX() {
-        return x;
+    public int getLigne() {
+        return ligne;
     }
 
-    public int getY() {
-        return y;
+    public int getColonne() {
+        return colonne;
     }
 
-    public void setX(int x) {
-        this.x = x;
+    public void setLigne(int ligne) {
+        this.ligne = ligne;
     }
 
-    public void setY(int y) {
-        this.y = y;
+    public void setColonne(int colonne) {
+        this.colonne = colonne;
     }
 
 }
